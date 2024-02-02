56e5729c3752a0de9859f4ba5b79c8f20f7402d0
Ashif Khan <ashifkhn02@gmail.com>
2024-02-02 21:42:29 +0530

    Initial Commit

---
+++ memeog/memeogApp.swift
@@ -0,0 +1,17 @@
+//
+//  memeogApp.swift
+//  memeog
+//
+//  Created by Ashif Khan on 02/02/24.
+//
+
+import SwiftUI
+
+@main
+struct memeogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
